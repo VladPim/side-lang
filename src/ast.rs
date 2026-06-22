cb3e58dd166c829a34975aeeae3685cb6f2d41f0
VladPim <pimenovvlad354@gmail.com>
2026-06-22 11:02:28 +0300

    Update ast.rs

--- src/ast.rs
+++ src/ast.rs
@@ -1,20 +1,22 @@
-/// Вся программа – набор функций
 #[derive(Debug)]
 pub struct Program {
     pub functions: Vec<Function>,
 }
 
-/// Одна функция: имя и список операторов тела
 #[derive(Debug)]
 pub struct Function {
     pub name: String,
+    pub params: Vec<String>,
     pub body: Vec<Stmt>,
 }
 
-/// Операторы
 #[derive(Debug)]
 pub enum Stmt {
     Let {
+        name: String,
+        value: Expr,
+    },
+    Assign {
         name: String,
         value: Expr,
     },
@@ -24,31 +26,44 @@ pub enum Stmt {
         then_body: Vec<Stmt>,
         else_body: Option<Vec<Stmt>>,
     },
+    While {
+        condition: Expr,
+        body: Vec<Stmt>,
+    },
+    Return(Expr),
+    Break,
+    Continue,
 }
 
-/// Выражения
 #[derive(Debug)]
 pub enum Expr {
     Number(i32),
     StringLiteral(String),
     Variable(String),
+    Input(String),
+    Call {
+        name: String,
+        args: Vec<Expr>,
+    },
     Binary {
         left: Box<Expr>,
         op: BinOp,
         right: Box<Expr>,
     },
+    Unary {
+        op: UnaryOp,
+        expr: Box<Expr>,
+    },
 }
 
 #[derive(Debug)]
 pub enum BinOp {
-    Add,
-    Sub,
-    Mul,
-    Div,
-    Eq,          // ==
-    NotEq,       // !=
-    Less,
-    Greater,
-    LessEq,      // <=
-    GreaterEq,   // >=
+    Add, Sub, Mul, Div,
+    Eq, NotEq, Less, Greater, LessEq, GreaterEq,
+    And, Or,          // логические
+}
+
+#[derive(Debug)]
+pub enum UnaryOp {
+    Not,
 }
